4611d1f0197a004294e83c1a199c3f66ce2bfbef
RK <serious8builder@gmail.com>
2021-04-03 01:21:13 +0900

    Fix typo

--- app/ots/database.py
+++ app/ots/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.geetenv('DATABSE_URL', "sqlite:///./ots.db")
+SQLALCHEMY_DATABASE_URL = os.getenv('DATABSE_URL', "sqlite:///./ots.db")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
